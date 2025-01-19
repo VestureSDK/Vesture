29e54b1a98227bbeca5fa26f4b4df707b2884b2b
Stephane Boulnois <s.boulnois@gmail.com>
2025-01-19 10:27:53 -0500

    Use logging generated message

--- src/Mediator/Ingot.Mediator.Engine/Pipeline/Extensions/MediatorLoggerExtensions.cs
+++ src/Mediator/Ingot.Mediator.Engine/Pipeline/Extensions/MediatorLoggerExtensions.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections;
-using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Ingot.Mediator.Engine.Pipeline.Internal;
 using Ingot.Mediator.Invocation;
 using Microsoft.Extensions.Logging;
@@ -11,182 +11,244 @@ namespace Ingot.Mediator.Engine.Pipeline.Extensions
     // Ingot:           ## {component} + ### {event}
     // Mediator:        10###
     // Mediator.Engine: 10001 -> 10999
-    internal static class MediatorLoggerExtensions
+    internal static partial class MediatorLoggerExtensions
     {
-        private static EventId Event(int id, string? name = null)
-        {
-            ThrowIfIdInvalid(id);
-            return new EventId(10000 + id, name);
-        }
+        internal const int BaseEventId = 10000;
 
-        [Conditional("DEBUG")]
-        private static void ThrowIfIdInvalid(int id)
-        {
-            if (id < 1 || id > 999)
-            {
-                throw new ArgumentException("An ingot compoent's event id must be in range [1, 99]", nameof(id));
-            }
-        }
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 1, 
+            EventName = nameof(InvocationPipelinesCached), 
+            Level = LogLevel.Trace, 
+            Message = "Invocation pipelines cached {Pipelines}")]
+        private static partial void InnerInvocationPipelineCompiled(this ILogger logger, Exception ex, 
+            IEnumerable<string> pipelines);
 
-        private static readonly Action<ILogger, IEnumerable<string>, Exception> s_invocationPipelineCompiled =
-            LoggerMessage.Define<IEnumerable<string>>(LogLevel.Trace,
-            Event(1, nameof(InvocationPipelinesCached)),
-            "Invocation pipelines cached {Pipelines}");
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelinesCached(this ILogger logger, IEnumerable<(Type RequestType, Type ResponseType)> registeredPipelines)
         {
-            if (!logger.IsEnabled(LogLevel.Trace))
+            if (logger.IsEnabled(LogLevel.Trace))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineCompiled(logger, 
+                    ex: null,
+                    pipelines: registeredPipelines.Select(p => $"{p.RequestType} -> {p.ResponseType}"));
             }
-
-            s_invocationPipelineCompiled(logger, registeredPipelines.Select(p => $"{p.RequestType} -> {p.ResponseType}"), null);
         }
 
-        private static readonly Action<ILogger, Type, Type, object, Exception> s_invocationPipelineFound =
-            LoggerMessage.Define<Type, Type, object>(LogLevel.Debug,
-            Event(2, nameof(InvocationPipelineFound)),
-            "Invocation pipeline found ({RequestType} -> {ResponseType}) for contract {Contract}");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 2,
+            EventName = nameof(InvocationPipelineFound),
+            Level = LogLevel.Debug,
+            Message = "Invocation pipeline found ({RequestType} -> {ResponseType}) for contract {Contract}")]
+        private static partial void InnerInvocationPipelineFound(this ILogger logger, Exception ex,
+            Type requestType,
+            Type responseType,
+            object contract);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelineFound<TResponse>(this ILogger logger, object request)
         {
-            if (!logger.IsEnabled(LogLevel.Debug))
+            if (logger.IsEnabled(LogLevel.Debug))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineFound(logger,
+                    ex: null,
+                    requestType: request.GetType(),
+                    responseType: typeof(TResponse),
+                    contract: request);
             }
-
-            s_invocationPipelineFound(logger, request.GetType(), typeof(TResponse), request, null);
         }
 
-        private static readonly Action<ILogger, Type, Type, object, Exception> s_invocationPipelineNotFound =
-            LoggerMessage.Define<Type, Type, object>(LogLevel.Debug,
-            Event(3, nameof(InvocationPipelineNotFound)),
-            "Invocation pipeline not found ({RequestType} -> {ResponseType}) for contract {Contract}; Falling back to NoOp invocation pipeline");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 3,
+            EventName = nameof(InvocationPipelineNotFound),
+            Level = LogLevel.Debug,
+            Message = "Invocation pipeline not found ({RequestType} -> {ResponseType}) for contract {Contract}; Falling back to NoOp invocation pipeline")]
+        private static partial void InnerInvocationPipelineNotFound(this ILogger logger, Exception ex,
+            Type requestType,
+            Type responseType,
+            object contract);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelineNotFound<TResponse>(this ILogger logger, object request)
         {
-            if (!logger.IsEnabled(LogLevel.Debug))
+            if (logger.IsEnabled(LogLevel.Debug))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineNotFound(logger,
+                    ex: null,
+                    requestType: request.GetType(),
+                    responseType: typeof(TResponse),
+                    contract: request);
             }
-
-            s_invocationPipelineNotFound(logger, request.GetType(), typeof(TResponse), request, null);
         }
 
-        private static readonly Action<ILogger, Type, Type, Type, int, Exception> s_invocationPipelineChainMiddlewareMatches =
-            LoggerMessage.Define<Type, Type, Type, int>(LogLevel.Trace,
-            Event(4, nameof(InvocationPipelineChainMiddlewareMatches)),
-            "{Middleware} matches ({RequestType} -> {ResponseType}) and will be added to the invocation pipeline chain at index {Index}");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 4,
+            EventName = nameof(InvocationPipelineChainMiddlewareMatches),
+            Level = LogLevel.Trace,
+            Message = "{MiddlewareType} matches ({RequestType} -> {ResponseType}) and will be added to the invocation pipeline chain at index {MiddlewareIndex}")]
+        private static partial void InnerInvocationPipelineChainMiddlewareMatches(this ILogger logger, Exception ex,
+            Type middlewareType,
+            Type requestType,
+            Type responseType,
+            int middlewareIndex);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelineChainMiddlewareMatches<TRequest, TResponse>(this ILogger logger, IMiddlewareInvocationPipelineItem middleware, IList middlewares)
         {
-            if (!logger.IsEnabled(LogLevel.Trace))
+            if (logger.IsEnabled(LogLevel.Trace))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineChainMiddlewareMatches(logger, 
+                    ex: null,
+                    middlewareType: middleware.MiddlewareType, 
+                    requestType: typeof(TRequest), 
+                    responseType: typeof(TResponse), 
+                    middlewareIndex: middlewares.Count);
             }
-
-            s_invocationPipelineChainMiddlewareMatches(logger, middleware.MiddlewareType, typeof(TRequest), typeof(TResponse), middlewares.Count, null);
         }
 
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 5,
+            EventName = nameof(InvocationPipelineChainMiddlewareDoesNotMatch),
+            Level = LogLevel.Trace,
+            Message = "{MiddlewareType} does not match ({RequestType} -> {ResponseType}) and will not be added to the invocation pipeline chain")]
+        private static partial void InnerInvocationPipelineChainMiddlewareDoesNotMatch(this ILogger logger, Exception ex,
+            Type middlewareType,
+            Type requestType,
+            Type responseType);
 
-        private static readonly Action<ILogger, Type, Type, Type, Exception> s_invocationPipelineChainMiddlewareDoesNotMatch =
-            LoggerMessage.Define<Type, Type, Type>(LogLevel.Trace,
-            Event(5, nameof(InvocationPipelineChainMiddlewareDoesNotMatch)),
-            "{Middleware} does not match ({RequestType} -> {ResponseType}) and will not be added to the invocation pipeline chain");
-
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelineChainMiddlewareDoesNotMatch<TRequest, TResponse>(this ILogger logger, IMiddlewareInvocationPipelineItem middleware)
         {
-            if (!logger.IsEnabled(LogLevel.Trace))
+            if (logger.IsEnabled(LogLevel.Trace))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineChainMiddlewareDoesNotMatch(logger,
+                    ex: null,
+                    middlewareType: middleware.MiddlewareType,
+                    requestType: typeof(TRequest),
+                    responseType: typeof(TResponse));
             }
-
-            s_invocationPipelineChainMiddlewareDoesNotMatch(logger, middleware.MiddlewareType, typeof(TRequest), typeof(TResponse), null);
         }
 
-        private static readonly Action<ILogger, Type, Type, int, IEnumerable<Type>, Exception> s_invocationPipelineChainCreated =
-            LoggerMessage.Define<Type, Type, int, IEnumerable<Type>>(LogLevel.Trace,
-            Event(6, nameof(InvocationPipelineChainCreated)),
-            "Invocation pipeline chain ({RequestType} -> {ResponseType}) created with {MiddlewaresCount} middlewares {Middlewares}");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 6,
+            EventName = nameof(InvocationPipelineChainCreated),
+            Level = LogLevel.Trace,
+            Message = "Invocation pipeline chain ({RequestType} -> {ResponseType}) created with {MiddlewareCount} middlewares {Middlewares}")]
+        private static partial void InnerInvocationPipelineChainCreated(this ILogger logger, Exception ex,
+            Type requestType,
+            Type responseType,
+            int middlewareCount,
+            IEnumerable<Type> middlewares);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvocationPipelineChainCreated<TRequest, TResponse>(this ILogger logger, IList<IMiddlewareInvocationPipelineItem> middlewares)
         {
-            if (!logger.IsEnabled(LogLevel.Trace))
+            if (logger.IsEnabled(LogLevel.Trace))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvocationPipelineChainCreated(logger,
+                    ex: null,
+                    requestType: typeof(TRequest),
+                    responseType: typeof(TResponse),
+                    middlewareCount: middlewares.Count,
+                    middlewares: middlewares.Select(m => m.MiddlewareType));
             }
-
-            s_invocationPipelineChainCreated(logger, typeof(TRequest), typeof(TResponse), middlewares.Count, middlewares.Select(m => m.MiddlewareType), null);
         }
 
-        private static readonly Action<ILogger, Type, Type?, Exception> s_prePipelineAndHandlerMiddlewareUnhandledException =
-            LoggerMessage.Define<Type, Type?>(LogLevel.Debug,
-            Event(7, nameof(PrePipelineAndHandlerMiddlewareUnhandledException)),
-            "Invocation pipeline chain ({RequestType} -> {ResponseType}) encountered an unhandled exception; an error has been added to the current invocation context");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 7,
+            EventName = nameof(PrePipelineAndHandlerMiddlewareUnhandledException),
+            Level = LogLevel.Debug,
+            Message = "Invocation pipeline chain ({RequestType} -> {ResponseType}) encountered an unhandled exception; an error has been added to the current invocation context")]
+        private static partial void InnerPrePipelineAndHandlerMiddlewareUnhandledException(this ILogger logger, Exception ex,
+            Type requestType,
+            Type? responseType);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void PrePipelineAndHandlerMiddlewareUnhandledException(this ILogger logger, IInvocationContext context, Exception exception)
         {
-            if (!logger.IsEnabled(LogLevel.Debug))
+            if (logger.IsEnabled(LogLevel.Debug))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerPrePipelineAndHandlerMiddlewareUnhandledException(logger,
+                    ex: exception,
+                    requestType: context.RequestType,
+                    responseType: context.ResponseType);
             }
-
-            s_prePipelineAndHandlerMiddlewareUnhandledException(logger, context.RequestType, context.ResponseType, exception);
         }
 
-        private static readonly Action<ILogger, Type, Type?, Exception> s_noHandlersRegisteredException =
-            LoggerMessage.Define<Type, Type?>(LogLevel.Debug,
-            Event(8, nameof(NoHandlersRegisteredException)),
-            "Invocation pipeline chain ({RequestType} -> {ResponseType}) does not have the required handler; an error has been added to the current invocation context");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 8,
+            EventName = nameof(NoHandlersRegisteredException),
+            Level = LogLevel.Debug,
+            Message = "Invocation pipeline chain ({RequestType} -> {ResponseType}) does not have the required handler; an error has been added to the current invocation context")]
+        private static partial void InnerNoHandlersRegisteredException(this ILogger logger, Exception ex,
+            Type requestType,
+            Type? responseType);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void NoHandlersRegisteredException(this ILogger logger, IInvocationContext context, Exception exception)
         {
-            if (!logger.IsEnabled(LogLevel.Debug))
+            if (logger.IsEnabled(LogLevel.Debug))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerPrePipelineAndHandlerMiddlewareUnhandledException(logger,
+                    ex: exception,
+                    requestType: context.RequestType,
+                    responseType: context.ResponseType);
             }
-
-            s_noHandlersRegisteredException(logger, context.RequestType, context.ResponseType, exception);
         }
 
-        private static readonly Action<ILogger, Type, Type, Type, Exception> s_invokingHandler =
-            LoggerMessage.Define<Type, Type, Type>(LogLevel.Debug,
-            Event(9, nameof(InvokingHandler)),
-            "Invoking {Handler} ({RequestType} -> {ResponseType})");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 9,
+            EventName = nameof(InvokingHandler),
+            Level = LogLevel.Debug,
+            Message = "Invoking {Handler} ({RequestType} -> {ResponseType})")]
+        private static partial void InnerInvokingHandler(this ILogger logger, Exception ex,
+            object handler,
+            Type requestType,
+            Type? responseType);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvokingHandler<TRequest, TResponse>(this ILogger logger, IInvocationHandler<TRequest, TResponse> handler)
         {
-            if (!logger.IsEnabled(LogLevel.Debug))
+            if (logger.IsEnabled(LogLevel.Debug))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvokingHandler(logger,
+                    ex: null,
+                    handler: handler,
+                    requestType: typeof(TRequest),
+                    responseType: typeof(TResponse));
             }
-
-            s_invokingHandler(logger, typeof(TRequest), typeof(TResponse), handler.GetType(), null);
         }
 
-        private static readonly Action<ILogger, Type, Type, Type, Exception> s_settingHandlerResultInContext =
-            LoggerMessage.Define<Type, Type, Type>(LogLevel.Trace,
-            Event(9, nameof(InvokedHandler)),
-            "Invoked {Handler} ({RequestType} -> {ResponseType}); response has been set into the current invocation context");
+        [LoggerMessage(
+            SkipEnabledCheck = true,
+            EventId = BaseEventId + 10,
+            EventName = nameof(InvokedHandler),
+            Level = LogLevel.Trace,
+            Message = "Invoked {Handler} ({RequestType} -> {ResponseType}); response has been set into the current invocation context")]
+        private static partial void InnerInvokedHandler(this ILogger logger, Exception ex,
+            object handler,
+            Type requestType,
+            Type? responseType);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void InvokedHandler<TRequest, TResponse>(this ILogger logger, IInvocationHandler<TRequest, TResponse> handler)
         {
-            if (!logger.IsEnabled(LogLevel.Trace))
+            if (logger.IsEnabled(LogLevel.Trace))
             {
-                // Early exit to avoid extra compute
-                return;
+                InnerInvokedHandler(logger,
+                    ex: null,
+                    handler: handler,
+                    requestType: typeof(TRequest),
+                    responseType: typeof(TResponse));
             }
-
-            s_settingHandlerResultInContext(logger, typeof(TRequest), typeof(TResponse), handler.GetType(), null);
         }
     }
 }

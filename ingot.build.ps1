c5dd67ff21da6974a0ea6b453e60224ecf9d9c25
Stephane Boulnois <s.boulnois@gmail.com>
2025-01-20 21:01:09 -0500

    Update ib

--- ingot.build.ps1
+++ ingot.build.ps1
@@ -1,3 +1,24 @@
+task Setup {
+    # Do the setup of the environment and repository
+    # It might also be used to migrate an existing
+    # setup to a new version of the setup
+}
+
+task Build {
+    $verbosity = "detailed"
+
+    # Builds the solution in Release configuration
+    dotnet clean ./src --verbosity $verbosity
+    dotnet restore ./src --verbosity $verbosity
+    dotnet build ./src -c Release --no-restore --verbosity $verbosity
+}
+
+task Test {
+    $verbosity = "detailed"
+
+    dotnet test ./src --no-build --verbosity $verbosity
+}
+
 task Src {
     & $(& "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property productPath -format json | ConvertFrom-Json)[0].productPath ./src/Ingot.sln -donotloadprojects
 }
@@ -21,8 +42,6 @@ task Src-Restore {
 task Src-Build {
     dotnet build ./src --verbosity detailed
 }
-
-task Build Src-Clean, Src-Format, Src-Restore, Src-Build
 
 task Docs-Clean {
     rm -Force -Recurse ./docs/docs/Mediator/References -ErrorAction SilentlyContinue
